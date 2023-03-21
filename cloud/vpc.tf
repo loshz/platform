e8dc2a211995eba5c91b5579ff5fcf06e23adb4e
Dan Bond <danbond@protonmail.com>
2023-03-21 10:10:18 -0700

    cloud: 2x vpc subnets
    Signed-off-by: Dan Bond <danbond@protonmail.com>

--- cloud/vpc.tf
+++ cloud/vpc.tf
@@ -11,66 +11,97 @@ resource "aws_vpc" "platform" {
   tags = { "Name" = local.name }
 }
 
-# Create a public subnet.
-resource "aws_subnet" "platform_public" {
+# Create 2 public subnets.
+resource "aws_subnet" "public" {
+  count = 2
+
   vpc_id                  = aws_vpc.platform.id
-  cidr_block              = cidrsubnet(var.vpc_cidr, 8, 0)
-  availability_zone       = data.aws_availability_zones.available.names[0]
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index + 1)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
 
-  tags = { "Name" = "public1-${data.aws_availability_zones.available.names[0]}" }
+  tags = { "Name" = "public${count.index + 1}-${data.aws_availability_zones.available.names[count.index]}" }
 }
 
-# Create a private subnet.
-resource "aws_subnet" "platform_private" {
-  vpc_id                  = aws_vpc.platform.id
-  cidr_block              = cidrsubnet(var.vpc_cidr, 8, 128)
-  availability_zone       = data.aws_availability_zones.available.names[0]
-  map_public_ip_on_launch = true
+# Create 2 private subnets.
+resource "aws_subnet" "private" {
+  count = 2
 
-  tags = { "Name" = "private1-${data.aws_availability_zones.available.names[0]}" }
+  vpc_id                  = aws_vpc.platform.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index + 3)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = false
+
+  tags = { "Name" = "private${count.index + 1}-${data.aws_availability_zones.available.names[count.index]}" }
 }
 
 # Create an IGW to enable internet-routable traffic.
-resource "aws_internet_gateway" "platform" {
+resource "aws_internet_gateway" "public" {
   vpc_id = aws_vpc.platform.id
 }
 
 # Create a single public route table to direct network traffic.
-resource "aws_route_table" "platform_public" {
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.platform.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.platform.id
+    gateway_id = aws_internet_gateway.public.id
   }
+
+  tags = { "Name" = "public" }
 }
 
 # Create 2 privte route tables to direct network traffic.
-resource "aws_route_table" "platform_private" {
+resource "aws_route_table" "private" {
+  count = 2
+
   vpc_id = aws_vpc.platform.id
+
+  tags = { "Name" = "private${count.index + 1}-${data.aws_availability_zones.available.names[count.index]}" }
 }
 
 # Associate the route table with the public subnets.
-resource "aws_route_table_association" "platform_public" {
-  subnet_id      = aws_subnet.platform_public.id
-  route_table_id = aws_route_table.platform_public.id
+resource "aws_route_table_association" "public" {
+  count = 2
+
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public.id
 }
 
 # Associate the route table with the privte subnets.
-resource "aws_route_table_association" "platform_private" {
-  subnet_id      = aws_subnet.platform_private.id
-  route_table_id = aws_route_table.platform_private.id
+resource "aws_route_table_association" "private" {
+  count = 2
+
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = element(aws_route_table.private.*.id, count.index)
 }
 
-# Create a security group with public egress.
-resource "aws_security_group" "platform" {
+# Create a security group with public ingress/egress.
+resource "aws_security_group" "load_balancer" {
   vpc_id = aws_vpc.platform.id
 
+  ingress {
+    protocol         = "tcp"
+    from_port        = 80
+    to_port          = 80
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    protocol         = "tcp"
+    from_port        = 443
+    to_port          = 443
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol         = "-1"
+    from_port        = 0
+    to_port          = 0
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
